355527d7715579c7d58a78ac6dac46932ea3a55b
V23Dan <victoordanilo@gmail.com>
2025-10-03 22:21:23 -0500

    AOS-10 Se agrego el ultimo metodo del servicio de Productos delete, ademas se creo la vista para el CRUD de productos

--- src/services/productService.js
+++ src/services/productService.js
@@ -3,7 +3,8 @@ import {
   getDocs,
   collection,
   doc,
-  updateDoc
+  updateDoc,
+  deleteDoc
 } from "firebase/firestore";
 import db from "../firebase.js";
 
@@ -68,4 +69,17 @@ export const updateProduct = async (id, updatedData) => {
     console.error("Error al actualizar producto: ", error);
     throw error;
   }
+};
+
+//deleteProduct:
+export const deleteProduct = async (id) => {
+    try {
+        const docRef = doc(db, "products", id);
+        await deleteDoc(docRef);
+
+        console.log("Producto eliminado con ID: ", id);
+    }catch (error){
+        console.error("Error al eliminar producto: ", error);
+        throw error;
+    }
 };
